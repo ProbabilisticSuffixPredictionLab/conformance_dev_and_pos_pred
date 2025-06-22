02bab1a5a79e32d405826188b8f3741d62b26b93
Henryk Mustroph <henryk.mustroph@tum.de>
2025-06-22 12:30:27 +0200

    start with evaluation

---
+++ src/evaluation/evalaution_metrics.py
@@ -0,0 +1,16 @@
+
+class EvaluationMetrics:
+    def __init__(self):
+        pass
+    
+    def precision_deviation():
+        pass
+    
+    def recall_deviation():
+        pass
+    
+    def precision_no_deviation():
+        pass
+    
+    def recall_no_deviation():
+        pass
